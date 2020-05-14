69cdb57f7d8203e2c7bedf9cd617dd7d2869d49a
18050674 <18050674@myrp.edu.sg>
2020-05-14 23:24:46 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demodatabase/TaskAdapter.java
@@ -0,0 +1,18 @@
+package com.myapplicationdev.android.demodatabase;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class TaskAdapter extends ArrayAdapter <Task> {
+    private ArrayList<Task> Tasks;
+    private Context context;
+
+    private TextView tv
+    private TextView tv
+    private TextView tv
+
+}
